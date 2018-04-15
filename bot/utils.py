339a9c77b1427b5d4f40b1b2d08817cdd5b78c0a
Patrik Turi <tpphysik@gmail.com>
2018-04-15 23:22:21 +0200

    Fix abs_path util path separator

--- bot/utils.py
+++ bot/utils.py
@@ -3,4 +3,4 @@ import os
 
 def abs_path(file, rel_path):
     dir_path = os.path.dirname(os.path.abspath(file))
-    return dir_path + os.altsep + rel_path;
+    return os.path.join(dir_path, rel_path);
